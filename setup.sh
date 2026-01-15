8704bad7182ebce01c6f3cb46f226116c90ab56d
Angel Cervera Roldan <48255007+angelcerveraroldan@users.noreply.github.com>
2026-01-15 09:21:25 +0000

    fedora

--- setup.sh
+++ setup.sh
@@ -8,12 +8,45 @@ log_info()
 	echo ">> $*"
 }
 
+# Check if we can use the arch installer
 ensure_arch() 
 {
     if ! command -v pacman >/dev/null 2>&1; then
-        echo "ERROR: This setup script currently only supports Arch Linux (pacman not found)." >&2
-        exit 1
+	    return 1
+    else
+	    return 0
     fi
+}
+
+# Check if we can use the fedora installer
+ensure_fedora()
+{
+    if ! command -v dnf >/dev/null 2>&1; then
+	    return 1
+    else
+	    return 0
+    fi
+
+}
+
+ensure_os()
+{
+	if ! (ensure_fedora || ensure_arch) then
+		log_info "Operating system not supported"
+		exit 1
+	fi
+}
+
+stow_arch_install()
+{
+	log_info "Installing stow (Arch)"
+	sudo pacman -S --needed --noconfirm stow
+}
+
+stow_fedora_install()
+{
+	log_info "Installing stow (Fedora)"
+	sudo dnf install stow
 }
 
 stow_install()
@@ -23,8 +56,14 @@ stow_install()
 		return
 	fi
 
-	log_info "Installing stow (Arch)"
-	sudo pacman -S --needed --noconfirm stow
+	# Install differently depending on package manager
+	if ensure_arch; then
+		stow_arch_install
+	elif ensure_fedora; then
+		stow_fedora_install
+	else
+		log_info "Error: Operating system not supported. pacman or dnf are needed"
+	fi
 
 	if ! command -v stow >/dev/null 2>&1
 	then
@@ -42,7 +81,14 @@ neovim_install()
 	fi
 
 	log_info "Installing neovim (Arch)"
-	sudo pacman -S --needed --noconfirm neovim
+
+	if ensure_arch
+	then
+		sudo pacman -S --needed --noconfirm neovim
+	elif ensure_fedora
+	then
+		sudo dnf install neovim
+	fi
 }
 
 # Optionally install dependencies to make all features in neovim work.
@@ -53,7 +99,7 @@ neovim_install()
 neovim_optional_deps()
 {
 	log_info "Installing optional Neovim dependencies (ripgrep, fd)"
-    sudo pacman -S --needed --noconfirm ripgrep fd
+	sudo pacman -S --needed --noconfirm ripgrep fd
 }
 
 neovim_setup()
@@ -70,6 +116,23 @@ neovim_config()
 	stow -Rvt "$HOME" nvim
 }
 
+
+keyd_minimum()
+{
+	if ! command -v make >/dev/null 2>&1
+	then
+		log_info "Error: Make not installed"
+		return
+	fi
+
+	if ! command -v systemctl >/dev/null 2>&1
+	then
+		log_info "Error: systemctl not found"
+		return
+	fi
+
+
+}
 
 keyd_install()
 {
@@ -109,10 +172,47 @@ keyd_setup()
 	keyd_config
 }
 
+sway_config()
+{
+	log_info "Stowing sway config"
+
+	cd "$REPO_DIR"
+	stow -Rvt "$HOME" nvim
+
+}
+
+sway_install()
+{
+	if command -v sway >/dev/null 2>&1
+	then
+		log_info "Sway already installed"
+		return
+	fi
+
+	log_info "Installing Sway"
+
+	if ensure_arch
+	then
+		sudo pacman -S sway
+	elif ensure_fedora
+	then
+		sudo dnf install sway
+	fi
+}
+
+sway_setup()
+{
+	if ! command -v sway >/dev/null 2>&1
+	then
+		sway_install
+	fi
+
+	sway_config
+}
+
 main()
 {
-	# For now, only arch works. Maybe add support for other OS later ...
-	ensure_arch
+	ensure_os
 
 	stow_install
 
